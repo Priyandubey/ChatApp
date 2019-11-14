e61a0a616b50bd36aa7d6c748e6d87eb34f3cd1b
priyandubey <Priyanshudubey74@gmail.com>
2019-11-14 22:00:43 +0530

    Fragment TabLayout

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -22,7 +22,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void login(View view){
 
-
+        Intent intent = new Intent(this,HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
 
     }
 
