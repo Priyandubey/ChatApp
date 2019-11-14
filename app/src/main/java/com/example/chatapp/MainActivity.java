c321f9e4a2031cec458d0eeb24e847ab341baefe
priyandubey <Priyanshudubey74@gmail.com>
2019-11-14 21:12:40 +0530

    linked login and signup page

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,4 +19,28 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    public void login(View view){
+
+    }
+
+    public void moveSignup(View view){
+
+        Intent intent = new Intent(this,SignupPage.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+
+
+    }
+    ///////////////// stack overflow input closer ///////////////
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (getCurrentFocus() != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+    /////////////////-----------------------------////////////////
+
 }
