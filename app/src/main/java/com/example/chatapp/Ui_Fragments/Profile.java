74c1cd1ef4e9b343a990f9b94ab39d273d11908a
priyandubey <Priyanshudubey74@gmail.com>
2019-12-12 16:17:03 +0530

    Strictly Portrait

--- app/src/main/java/com/example/chatapp/Ui_Fragments/Profile.java
+++ app/src/main/java/com/example/chatapp/Ui_Fragments/Profile.java
@@ -1,4 +1,4 @@
-package com.example.chatapp;
+package com.example.chatapp.Ui_Fragments;
 
 
 import android.content.Intent;
@@ -21,6 +21,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.chatapp.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
